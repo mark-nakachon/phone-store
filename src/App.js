f07295a1d751487c4ce594f85e98792244f23dc5
harish chennupati <y17cs027@rvrjc.ac.in>
2019-01-28 00:36:38 +0530

    some patch work and need to deploy

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ProductList from './components/ProductList';
 import Details from './components/Details';
 import Default from './components/Default';
 import Cart from './components/Cart';
+import Modal from './components/Modal';
 import { Switch, Route, Link } from 'react-router-dom';
 
 class App extends Component {
@@ -18,9 +19,8 @@ class App extends Component {
 					<Route exact path="/details" component={Details} />
 					<Route exact path="/cart" component={Cart} />
 					<Route component={Default} />
-					<Default />
-					<Cart />
 				</Switch>
+				<Modal />
 			</React.Fragment>
 		);
 	}
--- src/components/Details.js
+++ src/components/Details.js
@@ -1,10 +1,66 @@
 import React, { Component } from 'react';
-
+import { ProductConsumer } from '../Context';
+import { Link } from 'react-router-dom';
+import { ButtonContainer } from './ButtonContainer';
 export default class Details extends Component {
 	render() {
 		return (
 			<div>
-				<h3>hello from details</h3>
+				<ProductConsumer>
+					{(value) => {
+						const { id, company, img, info, price, title, inCart } = value.detailProduct;
+
+						return (
+							<div className="container py-5">
+								<div className="row">
+									<div className="col text-center mx-auto text-slanted text-blue">
+										<h1>{title}</h1>
+									</div>
+								</div>
+								{/* product info*/}
+								<div className="row">
+									<div className="col-10 mx-auto col-md-6 my-3">
+										<img src={img} className="img-fluid" alt="product" />
+									</div>
+									{/* product text*/}
+									<div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+										<h2>model :{title}</h2>
+										<h4 className="text-title text-uppercase text-muted mt-3 mb-2">
+											this is made by :<span>{company}</span>
+										</h4>
+										<h4 className="text-blue">
+											<strong>
+												price: <span>$</span>
+												{price}
+											</strong>
+										</h4>
+										<p className="text-capitalize font-weight-bold mt-3 mb-0">
+											<h3>some info about the product</h3>
+											<p className="tet-muted lead ">{info}</p>
+										</p>
+										{/*buttons*/}
+										<div className="d-flex justify-content-center">
+											<Link to="/" className="mr-4">
+												<ButtonContainer>back to products</ButtonContainer>
+											</Link>
+											<ButtonContainer
+												disabled={inCart ? true : false}
+												className="btn btn-lg btn-outline-secondary"
+												onClick={() => {
+													value.addToCart(id);
+													value.openModal(id);
+												}}
+												cart="true"
+											>
+												{inCart ? 'incart' : 'add to cart'}
+											</ButtonContainer>
+										</div>
+									</div>
+								</div>
+							</div>
+						);
+					}}
+				</ProductConsumer>
 			</div>
 		);
 	}
